7d827312c359d922c3c1faf8ec0660ae77a6da6f
David Xiang <dxxian@student.unimelb.edu.au>
2025-03-03 18:06:47 +1100

    tested whether the packages are installed and can run

---
+++ tester.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+output = "Hello World!"
+
+print("Hello ML")
+
+# NumPy Arrays
+arr = np.array([1, 2, 3, 4, 5])
+print("NumPy Array:", arr)
+
+# Mathematical Operations
+print("Array Sum:", np.sum(arr))
+print("Array Mean:", np.mean(arr))
+
+# Pandas Series
+ser = pd.Series([100, 200, 300, 400, 500])
+print("Pandas Series:")
+print(ser)
+
+
+
